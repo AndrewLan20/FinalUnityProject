4ab36016ac64e12678dcfc4306d42efea51a0fc3
AndrewLan20 <al0990557@student.francistuttle.edu>
2022-04-28 13:26:33 -0500

    creating missions for the game

--- New Unity Project/Assets/Scripts/MissionManager.cs
+++ New Unity Project/Assets/Scripts/MissionManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MissionManager : MonoBehaviour
 {
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -15,4 +20,46 @@ public class MissionManager : MonoBehaviour
     {
         
     }
+
+    public void startMission(Mission currentMission)
+    {
+
+    }
+
 }
+
+public class Objective
+{
+    public string objectiveText;
+    public bool hasBeenCompleted;
+
+    public void objective(string initObjText, bool initComplete)
+    {
+        objectiveText = initObjText;
+        hasBeenCompleted = initComplete;
+    }
+
+    public string GetobjectiveText()
+    {
+        return objectiveText;
+    }
+}
+
+public class Mission
+{
+    public string title;
+    public Objective[] missionObjectives;
+    public bool isActive;
+    public string completionText;
+
+    public void mission(string initTitle, string initCompText, Objective[] initObjectives)
+    {
+        title = initTitle;
+        completionText = initCompText;
+        missionObjectives = initObjectives;
+        
+    }
+}
+
+
+
